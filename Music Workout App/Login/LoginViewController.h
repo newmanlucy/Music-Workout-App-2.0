4df3a5d856b8a7da5ff8a6d6ccb4f309b2d9a62c
Lucy Newman <newmanlucy@ditto.cs.uchicago.edu>
2018-06-11 21:18:50 -0500

    Initial commit -- set up storyboards

---
+++ Music Workout App/Login/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  Music Workout App
+//
+//  Created by Lucy Newman on 6/11/18.
+//  Copyright © 2018 uchicago-cs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LoginViewController : NSObject
+
+@end
---
+++ Music Workout App/Login/LoginViewController.m
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.m
+//  Music Workout App
+//
+//  Created by Lucy Newman on 6/11/18.
+//  Copyright © 2018 uchicago-cs. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@implementation LoginViewController
+
+@end
