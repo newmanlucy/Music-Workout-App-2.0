ef7e38e3763b68fbc8c56b7ef5ae35c64ca10b89
Lucy Newman <newmanlucy@vulpix.cs.uchicago.edu>
2018-06-30 13:37:37 -0500

    playing and changing tempo of music

--- Music Workout App/Music/MakeMusicViewController.swift
+++ Music Workout App/Music/MakeMusicViewController.swift
@@ -7,10 +7,44 @@
 //
 
 import UIKit
+import AVFoundation
 
 class MakeMusicViewController: UIViewController {
-
+   
+    @IBOutlet weak var PauseOrPlayButton: UIButton!
+    
+    @IBOutlet weak var RateLabel: UILabel!
+    
+    @IBAction func PauseOrPlay(_ sender: Any) {
+        self.togglePlay()
+    }
+    
+    @IBAction func ChangeRateSlider(_ sender: UISlider) {
+        var rate: Float = 1.0
+        if (sender.value > 0) {
+            rate = 1 + sender.value
+        } else if (sender.value < 0) {
+            rate = 1 / (1 - sender.value)
+        }
+        self.RateLabel.text = String(rate)
+        player.rate = rate
+    }
+    
+    var player = AVAudioPlayer()
+    var isPlaying = false
+    
     override func viewDidLoad() {
+        let path = Bundle.main.path(forResource: "franz-liszt-liebestraum-3", ofType: "mp3")
+        do {
+            try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: path!))
+        } catch {
+            print("error: file not loaded")
+        }
+        player.prepareToPlay()
+        player.enableRate = true
+        
+        self.togglePlay()
+        
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
@@ -21,7 +55,18 @@ class MakeMusicViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    func togglePlay() {
+        if (self.isPlaying) {
+            player.pause()
+            self.PauseOrPlayButton.setTitle("Play", for: .normal)
+            
+        } else {
+            player.play()
+            self.PauseOrPlayButton.setTitle("Pause", for: .normal)
+        }
+        self.isPlaying = !self.isPlaying
+    }
+    
     /*
     // MARK: - Navigation
 
